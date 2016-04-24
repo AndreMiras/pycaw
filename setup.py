93f8332c2bd481db5ac3f358d82380e4f425165a
Andre Miras <andre.miras@gmail.com>
2016-04-24 10:56:30 -0700

    Setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pycaw',
+      version='1.0',
+      description='Python Core Audio Windows Library',
+      author='Andre Miras',
+      url='https://github.com/AndreMiras/pycaw',
+      packages=['pycaw'])
