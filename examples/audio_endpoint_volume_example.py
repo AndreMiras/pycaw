9933e2536a1d8820e579695c26fe02f4c99874eb
Andre Miras <andre.miras@gmail.com>
2016-05-06 22:42:38 +0200

    Added IAudioEndpointVolume usage example

---
+++ examples/audio_endpoint_volume_example.py
@@ -0,0 +1,20 @@
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw import AudioUtilities, IAudioEndpointVolume
+
+
+def main():
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(
+        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    print "volume.GetMute():", volume.GetMute()
+    print "volume.GetMasterVolumeLevel():", volume.GetMasterVolumeLevel()
+    print "volume.GetVolumeRange():", volume.GetVolumeRange()
+    print "volume.SetMasterVolumeLevel()"
+    volume.SetMasterVolumeLevel(-20.0, None)
+    print "volume.GetMasterVolumeLevel():", volume.GetMasterVolumeLevel()
+
+
+if __name__ == "__main__":
+    main()
