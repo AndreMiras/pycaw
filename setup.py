93c167ecb439263ecfb954c248beaa3bb6ef734e
Andre Miras <andre.miras@gmail.com>
2018-12-26 12:00:33 +0100

    v20181226

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pycaw',
-      version='20160929',
+      version='20181226',
       description='Python Core Audio Windows Library',
       author='Andre Miras',
       url='https://github.com/AndreMiras/pycaw',
