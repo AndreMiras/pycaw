bbb969fbf232c1b82bd2f48a5e5ab019dabcc76b
mmxfguerin <francis.guerin@mirametrix.com>
2019-07-25 16:58:50 -0400

    Changed the enum34 dependency to enum-compat so that it only installs enum34 if the Python version is <3.6. It's causing issues when building an executable with PyInstaller on 3.6.

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from distutils.core import setup
 
 setup(name='pycaw',
-      version='20181226',
+      version='20190725',
       description='Python Core Audio Windows Library',
       author='Andre Miras',
       url='https://github.com/AndreMiras/pycaw',
       packages=['pycaw'],
-      install_requires=['comtypes', 'enum34', 'psutil', 'future'])
+      install_requires=['comtypes', 'enum-compat', 'psutil', 'future'])
