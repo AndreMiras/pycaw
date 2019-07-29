bc7060878f511f4084c4eee8d18e1d69b5583cb8
Andre Miras <AndreMiras@users.noreply.github.com>
2019-07-29 23:45:29 +0200

    Update setup.py fixes lint and Python version

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@
 
 from distutils.core import setup
 
+install_requires = [
+    'comtypes', 'enum34;python_version<"3.4"', 'psutil', 'future']
 setup(name='pycaw',
       version='20181226',
       description='Python Core Audio Windows Library',
       author='Andre Miras',
       url='https://github.com/AndreMiras/pycaw',
       packages=['pycaw'],
-      install_requires=['comtypes', 'enum34;python_version<"3.6"', 'psutil', 'future'])
+      install_requires=install_requires)
