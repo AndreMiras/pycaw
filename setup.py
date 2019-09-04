0787d245ce9d23d60524d31ce63f2f3bae5b1fb1
Andre Miras <andre.miras@gmail.com>
2019-09-04 18:46:15 +0200

    Adds setup `long_description`

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
+import os
 
-from distutils.core import setup
+from setuptools import setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
 
 install_requires = [
     'comtypes', 'enum34;python_version<"3.4"', 'psutil', 'future']
 setup(name='pycaw',
       version='20181226',
       description='Python Core Audio Windows Library',
+      long_description=read('README.md'),
+      long_description_content_type='text/markdown',
       author='Andre Miras',
       url='https://github.com/AndreMiras/pycaw',
       packages=['pycaw'],
