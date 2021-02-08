511b0f0d42f3d0338e2dc35e3fa1f8474a92ced8
Justin Patrin <papercrane@reversefold.com>
2021-02-08 09:13:19 -0700

    Cleanup

--- tests/test_core.py
+++ tests/test_core.py
@@ -28,9 +28,7 @@ def captured_output():
 class TestCore(unittest.TestCase):
 
     def test_session_unicode(self):
-        """
-        Makes sure printing a session doesn't crash.
-        """
+        """Makes sure printing a session doesn't crash."""
         with captured_output() as (out, err):
             sessions = AudioUtilities.GetAllSessions()
             print("sessions: %s" % sessions)
@@ -39,9 +37,7 @@ class TestCore(unittest.TestCase):
                 print("session.Process: %s" % session.Process)
 
     def test_device_unicode(self):
-        """
-        Makes sure printing a device doesn't crash.
-        """
+        """Makes sure printing a device doesn't crash."""
         with captured_output() as (out, err):
             devices = AudioUtilities.GetAllDevices()
             print("devices: %s" % devices)
@@ -49,20 +45,23 @@ class TestCore(unittest.TestCase):
                 print("device: %s" % device)
 
     def test_device_failed_properties(self):
-        """
-        Makes sure printing a device doesn't crash.
-        """
-
+        """Test that failed properties do not crash the script"""
+        dev = mock.Mock()
+        dev.GetId = mock.Mock(return_value="id")
+        dev.GetState = mock.Mock(return_value=AudioDeviceState.Active)
+        store = mock.Mock()
+        store.GetCount = mock.Mock(return_value=1)
+        store.GetAt = mock.Mock(return_value="pk")
+        store.GetValue = mock.Mock(
+            side_effect=_ctypes.COMError(None, None, None)
+        )
+        dev.OpenPropertyStore = mock.Mock(return_value=store)
         with captured_output() as (out, err):
-            dev = mock.Mock()
-            dev.GetId = mock.Mock(return_value="id")
-            dev.GetState = mock.Mock(return_value=AudioDeviceState.Active)
-            store = mock.Mock()
-            store.GetCount = mock.Mock(return_value=3)
-            store.GetAt = mock.Mock(return_value="pk")
-            store.GetValue = mock.Mock(side_effect=_ctypes.COMError(None, None, None))
-            dev.OpenPropertyStore = mock.Mock(return_value=store)
-            audev = AudioUtilities.CreateDevice(dev)
+            AudioUtilities.CreateDevice(dev)
+        self.assertTrue(
+            "UserWarning: COMError attempting to get property 0 from device"
+            in err.getvalue()
+        )
 
     def test_getallsessions_reliability(self):
         """
