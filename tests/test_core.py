038bb7a936f6bcac6c8bef59ac3e4296be616b35
papercrane <papercrane@reversefold.com>
2021-02-08 20:21:18 -0700

    pycaw #15: Ignore properties that cause a COMError (#30)
    * pycaw #15: Ignore properties that cause a COMError
    * Cleanup

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,10 +2,12 @@
 Verifies core features run as expected.
 """
 from __future__ import print_function
+import _ctypes
 import sys
 import unittest
+from unittest import mock
 from contextlib import contextmanager
-from pycaw.pycaw import AudioUtilities
+from pycaw.pycaw import AudioDeviceState, AudioUtilities
 try:
     from StringIO import StringIO
 except ImportError:
@@ -26,9 +28,7 @@ def captured_output():
 class TestCore(unittest.TestCase):
 
     def test_session_unicode(self):
-        """
-        Makes sure printing a session doesn't crash.
-        """
+        """Makes sure printing a session doesn't crash."""
         with captured_output() as (out, err):
             sessions = AudioUtilities.GetAllSessions()
             print("sessions: %s" % sessions)
@@ -37,14 +37,31 @@ class TestCore(unittest.TestCase):
                 print("session.Process: %s" % session.Process)
 
     def test_device_unicode(self):
-        """
-        Makes sure printing a device doesn't crash.
-        """
+        """Makes sure printing a device doesn't crash."""
         with captured_output() as (out, err):
             devices = AudioUtilities.GetAllDevices()
             print("devices: %s" % devices)
             for device in devices:
                 print("device: %s" % device)
+
+    def test_device_failed_properties(self):
+        """Test that failed properties do not crash the script"""
+        dev = mock.Mock()
+        dev.GetId = mock.Mock(return_value="id")
+        dev.GetState = mock.Mock(return_value=AudioDeviceState.Active)
+        store = mock.Mock()
+        store.GetCount = mock.Mock(return_value=1)
+        store.GetAt = mock.Mock(return_value="pk")
+        store.GetValue = mock.Mock(
+            side_effect=_ctypes.COMError(None, None, None)
+        )
+        dev.OpenPropertyStore = mock.Mock(return_value=store)
+        with captured_output() as (out, err):
+            AudioUtilities.CreateDevice(dev)
+        self.assertTrue(
+            "UserWarning: COMError attempting to get property 0 from device"
+            in err.getvalue()
+        )
 
     def test_getallsessions_reliability(self):
         """
