8baf7f4ba5f10f30399da7a7ff6ceb3ccd839fe3
Andre Miras <andre.miras@gmail.com>
2022-04-17 02:03:48 +0200

    :wrench: Exclude tests and example packages, refs #63

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setup(name='pycaw',
       long_description_content_type='text/markdown',
       author='Andre Miras',
       url='https://github.com/AndreMiras/pycaw',
-      packages=find_packages(),
+      packages=find_packages(exclude=("tests", "examples")),
       install_requires=install_requires)
