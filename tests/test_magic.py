4ba2ab464fe6d3fd00f9fd7bd4bafe89db4e98da
Andre Miras <andre.miras@gmail.com>
2022-04-18 11:08:29 +0000

    :white_check_mark: test_activate_magic(), refs #45

--- tests/test_magic.py
+++ tests/test_magic.py
@@ -1,3 +1,4 @@
+import warnings
 from unittest import mock
 
 from pycaw.magic import MagicApp, MagicManager, MagicSession
@@ -11,7 +12,6 @@ def patch_atexit_register():
 class TestMagic:
     def test_init(self):
         app_execs = {"msedge.exe"}
-        # prevent MagicManager.clean_up() call as it seems to misbehave with tests
         with patch_atexit_register() as m_register:
             magic = MagicApp(app_execs)
         assert magic.app_execs == app_execs
@@ -35,3 +35,26 @@ class TestMagicManager:
         assert hasattr(MagicManager, "magic_apps") is False
         assert hasattr(MagicManager, "magic_sessions") is False
         assert MagicManager.magic_activated is None
+
+    def test_activate_magic(self):
+        MagicManager.magic_activated = None
+        app_execs = {"msedge.exe"}
+        with patch_atexit_register() as m_register, warnings.catch_warnings(
+            record=True
+        ) as w:
+            MagicApp(app_execs)
+        assert m_register.called
+        assert len(w) == 1
+        MagicManager.unregister_all()
+        with warnings.catch_warnings(record=True) as w:
+            MagicManager.activate_magic()
+        assert len(w) == 1
+        assert "<MagicManager/> was already activated an closed." in str(w[-1].message)
+        assert MagicManager.magic_activated is True
+        with warnings.catch_warnings(record=True) as w:
+            MagicManager.activate_magic()
+        assert len(w) == 1
+        assert "cannot activate MagicManager. MagicManager is already active!" in str(
+            w[-1].message
+        )
+        assert MagicManager.magic_activated is True
