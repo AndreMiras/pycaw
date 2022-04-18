b7ca3f8552637a3463544bec455125bc0c181562
Andre Miras <andre.miras@gmail.com>
2022-04-18 10:26:33 +0000

    :white_check_mark: test_unregister_all(), refs #45

--- tests/test_magic.py
+++ tests/test_magic.py
@@ -1,11 +1,21 @@
+from unittest import mock
+
 from pycaw.magic import MagicApp, MagicManager, MagicSession
+
+
+def patch_atexit_register():
+    """Prevent MagicManager.clean_up() call as it seems to misbehave with tests."""
+    return mock.patch("atexit.register")
 
 
 class TestMagic:
     def test_init(self):
         app_execs = {"msedge.exe"}
-        magic = MagicApp(app_execs)
+        # prevent MagicManager.clean_up() call as it seems to misbehave with tests
+        with patch_atexit_register() as m_register:
+            magic = MagicApp(app_execs)
         assert magic.app_execs == app_execs
+        assert m_register.called
 
 
 class TestMagicManager:
@@ -13,3 +23,15 @@ class TestMagicManager:
         assert MagicManager.MagicSessionConfigured is None
         MagicManager.magic_session(MagicSession)
         assert MagicManager.MagicSessionConfigured == (MagicSession, (), {})
+
+    def test_unregister_all(self):
+        assert MagicManager.magic_apps is not None
+        assert MagicManager.str() == (
+            "<MagicManager magic_apps='1' magic_sessions='1'"
+            " active_mrs='1' trash_mrs='0'/>"
+        )
+        MagicManager.unregister_all()
+        assert MagicManager.str() == "unactive MagicManager"
+        assert hasattr(MagicManager, "magic_apps") is False
+        assert hasattr(MagicManager, "magic_sessions") is False
+        assert MagicManager.magic_activated is None
