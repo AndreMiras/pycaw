7b9dac2bd1d46e666ba9a6381baf285b02664343
Andre Miras <andre.miras@gmail.com>
2023-03-22 16:16:30 +0100

    :bookmark: v20230322

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 install_requires = ["comtypes", "psutil"]
 setup(
     name="pycaw",
-    version="20220416.dev0",
+    version="20230322",
     description="Python Core Audio Windows Library",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
